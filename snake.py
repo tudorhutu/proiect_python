17dd8c003f734b4f2a22ade0c06a9b21f0f10b4b
tudorhutu <tudorhutu2012@gmail.com>
2021-12-06 22:21:48 +0200

    game reset on death

--- snake.py
+++ snake.py
@@ -2,79 +2,78 @@ import pygame
 import random
 pygame.init()
 
+def all_of_it():
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    SNAKE_LENGTH=3
+    SCREEN_WIDTH=1080
+    SCREEN_HEIGHT=720
+    # Set the width and height of each snake segment and point
+    segment_width = 18
+    segment_height = 18
+    point_witdh = 20
+    point_height = 20
+    # Margin between each segment
+    segment_margin = 2
+    
+    # Set initial speed
+    x_change = segment_width + segment_margin
+    y_change = 0
+    
+    
+    class Segment(pygame.sprite.Sprite):
+        def __init__(self, x, y):
+            super().__init__()
+    
+            self.image = pygame.Surface([segment_width, segment_height])
+            self.image.fill(WHITE)
+    
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-SNAKE_LENGTH=3
-SCREEN_WIDTH=1080
-SCREEN_HEIGHT=720
-# Set the width and height of each snake segment and point
-segment_width = 18
-segment_height = 18
-point_witdh = 20
-point_height = 20
-# Margin between each segment
-segment_margin = 2
- 
-# Set initial speed
-x_change = segment_width + segment_margin
-y_change = 0
- 
- 
-class Segment(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
- 
-        self.image = pygame.Surface([segment_width, segment_height])
-        self.image.fill(WHITE)
- 
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+    class Point(pygame.sprite.Sprite):
+        def __init__(self, x, y):
+            super().__init__()
+    
+            self.image = pygame.Surface([segment_width, segment_height])
+            self.image.fill(GREEN)
+    
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+    
+    #initialising the screen
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    pygame.display.set_caption('Snake')
+    allspriteslist = pygame.sprite.Group()
+    pointgroup = pygame.sprite.Group()
+    
+    #initialising the snake
+    snake_segments = []
+    for i in range(SNAKE_LENGTH):
+        x = 20
+        y = 20
+        segment = Segment(x, y)
+        snake_segments.append(segment)
+        allspriteslist.add(segment)
 
-class Point(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
- 
-        self.image = pygame.Surface([segment_width, segment_height])
-        self.image.fill(GREEN)
- 
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
- 
-#initialising the screen
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-pygame.display.set_caption('Snake')
-allspriteslist = pygame.sprite.Group()
-pointgroup = pygame.sprite.Group()
- 
-#initialising the snake
-snake_segments = []
-for i in range(SNAKE_LENGTH):
-    x = 20
-    y = 20
-    segment = Segment(x, y)
-    snake_segments.append(segment)
-    allspriteslist.add(segment)
+    #initialising the points
+    point = Point(100,100)
+    pointgroup.add(point)
+    
+    clock = pygame.time.Clock()
+    done = False
 
-#initialising the points
-point = Point(100,100)
-pointgroup.add(point)
- 
-clock = pygame.time.Clock()
-done = False
-quit= False
 
-while not quit:
     while not done:
         point_acquired = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-    
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = (segment_width + segment_margin) * -1
@@ -91,7 +90,7 @@ while not quit:
 
         point_hit_list = pygame.sprite.spritecollide(segment, pointgroup, False)
         if(point_hit_list): point_acquired=True
-    
+
         #"animation" and screenwrap
         if not point_acquired:
             old_segment = snake_segments.pop()
@@ -111,11 +110,11 @@ while not quit:
         #collision and game over
         blocks_hit_list = pygame.sprite.spritecollide(segment, allspriteslist, True)
         if(blocks_hit_list): 
-            done=True
+            all_of_it()
             
 
 
-    
+
         #drawing all elements on the screen
         snake_segments.insert(0, segment)
         allspriteslist.add(segment)
@@ -126,5 +125,7 @@ while not quit:
         #gamespeed
         clock.tick(len(snake_segments))
         print(len(snake_segments))
-    
-pygame.quit()
+        
+    pygame.quit()
+
+all_of_it()
